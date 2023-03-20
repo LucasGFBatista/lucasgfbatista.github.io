bd875cf8a64f5c7bdbd9ed5561c9e2051e377179
Lucas Gonçalves <lucas.gf.batista@gmail.com>
2023-03-20 18:37:11 -0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+    $to = "lucasgf.batista+contanto@gmail.com"; // destinatário
+    
+    $subject = "Contato via Portifolio";
+    
+    $message = $_POST['message']." - ".$_POST['name'];
+
+    $email = $_POST['email']; //remetente
+
+
+    $header[] = 'MIME-Version: 1.0';
+    $header[] = 'Content-type: text/html; charset=iso-8859-1';
+    $header[] = 'To: $to';
+    $header[] = 'From: $email';
+
+    $status = mail($to, $subject, $message, implode("\r\n", $header));
+
+
+    if($status==true){
+        print "Mensagem foi enviada com sucesso!";    
+
+    } else {
+    print "Mensagemnão pode ser enviada.";
+    
+    }
+
+?>

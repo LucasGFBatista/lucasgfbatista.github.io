eff41d1c296131435ca620344042d4ddc9ae1d74
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-03-20 22:25:48 -0300

    alterado php

--- contact.php
+++ contact.php
@@ -7,21 +7,20 @@
 
     $email = $_POST['email']; //remetente
 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $header[] = 'MIME-Version: 1.0';
+        $header[] = 'Content-type: text/html; charset=iso-8859-1';
+        $header[] = "To: $to";
+        $header[] = "From: $email";
 
-    $header[] = 'MIME-Version: 1.0';
-    $header[] = 'Content-type: text/html; charset=iso-8859-1';
-    $header[] = "To: $to";
-    $header[] = "From: $email";
+        $status = mail($to, $subject, $message, implode("\r\n", $header));
 
-    $status = mail($to, $subject, $message, implode("\r\n", $header));
-
-
-    if($status==true){
-        print "Mensagem foi enviada com sucesso!";    
-
+        if($status == true) {
+            print "Mensagem foi enviada com sucesso!";    
+        } else {
+            print "Mensagem não pode ser enviada.";
+        }
     } else {
-    print "Mensagemnão pode ser enviada.";
-    
+        print "Endereço de e-mail inválido.";
     }
-
 ?>
